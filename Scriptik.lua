a899e54181a678bba88564874f14c5e7af8fc47d
NikitosZuev <komrovnikita@gmail.com>
2025-04-21 21:29:39 +0700

    Update Scriptik.lua

--- Scriptik.lua
+++ Scriptik.lua
@@ -1,4 +1,4 @@
-local Key = "Zuev"
+local Key = "Zuv"
 local UserInputService = game:GetService("UserInputService")
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
